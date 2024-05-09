bb8b311aec2ad557896e097dc4b617f134c133c4
Mailet2997 <mailet2997@gmail.com>
2024-05-09 08:17:59 -0500

    funcion-p

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+//Funcion para calcular
+function calcular(num1, num2, op){
+    switch(op){
+        case"+":
+        return sumar(num1, num2);
+        case"-":
+        return restar(num1,num2);
+        case"/":
+        return dividir(num1,num2);
+        case"*":
+        return multi(num1,num2);
+        default:
+            return"Operacion no valida"
+
+    }
+}
+
+//Leison
+
+//Valeria
+
+//Dumar
